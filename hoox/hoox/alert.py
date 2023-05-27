56697e396510c1800efa3338bba93ab8391e331b
jango_blockchained <jan.goischke@gmail.com>
2023-05-27 04:19:02 +0000

    .

--- hoox/hoox/alert.py
+++ hoox/hoox/alert.py
@@ -14,13 +14,23 @@ def hoox():
         request_data = json.loads(frappe.request.data)
         secret_hash = request_data.get('secret_hash')
         user_creds = get_user_credentials(secret_hash)
-        if user_creds.enabled:
-            handle_alert(request_data, user_creds)
-        else:
-            raise ValidationError("Invalid Secret Hash")
+        # --
+        if request_data.get('action') in ['buy', 'sell', 'close']:
+            if user_creds.enabled:
+                handle_alert(request_data, user_creds)
+            else:
+                raise ValidationError("Invalid Secret Hash")
+        # --
+        if telegram in request_data:
+            send_to_telegram(user_creds.user, request_data.get('telegram'))
+        # --
+        if haas in request_data:
+            send_to_haas(request_data.get('haas'))
+    # --
     except (DoesNotExistError, ValidationError) as e:
         logging.error(f"An error occurred in hoox: {str(e)}")
         frappe.throw(str(e), frappe.AuthenticationError)
+    # --
     except Exception as e:
         logging.error(f"An unexpected error occurred in hoox: {str(e)}")
         frappe.throw("An unexpected error occurred", frappe.AuthenticationError)
@@ -30,7 +40,7 @@ def handle_alert(request_data, user_creds, is_retry=False):
     try:
         # Logging
         logging.info(f"Incoming request from TradingView: {request_data}")
-
+        # --
         if not is_retry: 
             # Create a new Trade doc
             trade = frappe.get_doc({
@@ -45,6 +55,11 @@ def handle_alert(request_data, user_creds, is_retry=False):
                 "market_type": request_data.get("market_type")
             })
             trade.insert(ignore_permissions=True)
+        else:
+            trade = frappe.get_last_doc('Trades', {
+                'secret_hash': request_data.get('secret_hash'),
+                'state': ['!=', 'Success']
+            })
 
         # Call to the exchange
         exchange_response = execute_order(
@@ -55,21 +70,21 @@ def handle_alert(request_data, user_creds, is_retry=False):
             request_data.get("quantity"),
             request_data.get("market_type"),
             user_creds)
-        
+               
+        trade.append("outgoing_requests", {
+            "doctype": "Outgoing Requests",
+            "parent_trade": trade.name,
+            "method": request_data.get('action'),
+            "url": user_creds.exchange,
+            "params": json.dumps(request_data),
+            "response": exchange_response,
+            "timestamp": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+        })
+        trade.save()
+
         send_to_telegram(user_creds.user, f"Order executed: {exchange_response}")
         logging.info(f"Outgoing request to Exchange: {request_data}")
         logging.info(f"Exchange response: {exchange_response}")
-
-        if not is_retry:
-            # Append to the 'OutgoingRequest' child table only on first attempt
-            trade.append("outgoing_requests", {
-                "doctype": "Outgoing Requests",
-                "api_method": request_data.get('action'),
-                "api_url": user_creds.exchange,
-                "params": json.dumps(request_data),
-                "timestamp": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-            })
-            trade.save()
 
     except Exception as e:
         print(f"An error occurred: {e}")
