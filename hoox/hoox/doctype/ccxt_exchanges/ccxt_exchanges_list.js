55a41489cc9754932d91204201adf2898a315498
jango_blockchained <jan.goischke@gmail.com>
2023-05-29 19:06:06 +0000

    .

--- hoox/hoox/doctype/ccxt_exchanges/ccxt_exchanges_list.js
+++ hoox/hoox/doctype/ccxt_exchanges/ccxt_exchanges_list.js
@@ -2,8 +2,8 @@ frappe.listview_settings['CCXT Exchanges'] = {
 	add_fields: ['exchange_id', 'exchange_name', 'precision_mode', 'rate_limit', 'testnet'],
 	filters: [['docstatus', '=', 1]],
 	onload: function (listview) {
-		listview.page.add_menu_item('Custom Button', async function () {
-			await frappe.call({
+		listview.page.add_menu_item('Custom Button', function () {
+			frappe.call({
 				method: "hoox.hoox.exchange.sync_exchanges"
 			});
 		});
