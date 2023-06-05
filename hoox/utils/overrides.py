b26e6b5952f6bac0d579f5f581e16de9d4a31756
jango_blockchained <jan.goischke@gmail.com>
2023-06-05 13:10:08 +0200

    Added supervisor tools

---
+++ hoox/utils/overrides.py
@@ -0,0 +1,79 @@
+import frappe
+from telegram.ext import Dispatcher, ExtBot, Updater
+from frappe_telegram.handlers.logging import log_outgoing_message
+
+
+"""
+For each incoming Update, we will have frappe initialized.
+We will override Dispatcher and Bot instance
+- Dispatcher is overridden for initializing frappe for each incoming Update
+- Bot is overridden for loggign outgoing messages
+NOTE:
+    Class attributes that starts with __ is Mangled
+"""
+
+
+class FrappeTelegramExtBot(ExtBot):
+
+    # The name of the active FrappeTelegramBot
+    telegram_bot: str
+
+    @classmethod
+    def make(cls, telegram_bot: str, updater: Updater):
+        bot = updater.bot
+        new_bot = cls(
+            bot.token,
+            bot.base_url,
+            request=updater._request,
+            defaults=bot.defaults,
+            arbitrary_callback_data=bot.arbitrary_callback_data,
+        )
+        new_bot.base_url = bot.base_url
+        new_bot.base_file_url = bot.base_file_url
+        new_bot.private_key = bot.private_key
+        new_bot.telegram_bot = telegram_bot
+        return new_bot
+
+    def _message(self, *args, **kwargs):
+        result = super()._message(*args, **kwargs)
+        log_outgoing_message(self.telegram_bot, result)
+        return result
+
+
+class FrappeTelegramDispatcher(Dispatcher):
+
+    # The Frappe Site
+    site: str
+
+    @classmethod
+    def make(cls, site, updater):
+        dispatcher = updater.dispatcher
+        return cls(
+            site,
+            updater.bot,
+            updater.update_queue,
+            job_queue=updater.job_queue,
+            workers=dispatcher.workers,
+            # Class attributes that starts with __ is Mangled
+            exception_event=updater._Updater__exception_event,
+            persistence=dispatcher.persistence,
+            use_context=dispatcher.use_context,
+            context_types=dispatcher.context_types,
+        )
+
+    def __init__(self, site, *args, **kwargs):
+        self.site = site
+        print("Using Patched Frappe Telegram Dispatcher ✅")
+        return super().__init__(*args, **kwargs)
+
+    def process_update(self, update: object) -> None:
+        try:
+            frappe.init(site=self.site)
+            frappe.flags.in_telegram_update = True
+            frappe.connect()
+            super().process_update(update=update)
+        except BaseException:
+            frappe.log_error(title="Telegram Process Update Error", message=frappe.get_traceback())
+        finally:
+            frappe.db.commit()
+            frappe.destroy()
---
+++ hoox/utils/supervisor.py
@@ -0,0 +1,139 @@
+import os
+import configparser
+
+import frappe
+from .bench import get_bench_path, get_bench_name, get_site_path
+
+"""
+supervisor.conf follows configparser format (Win-INI style)
+We will have a new process group for all the telegram-bots
+"""
+
+
+def add_supervisor_entry(
+        telegram_bot, polling=False, poll_interval=0,
+        webhook=False, webhook_port=0, webhook_url=None):
+
+    # Validate telegram_bot exists
+    if not frappe.db.exists("Telegram Bot", telegram_bot):
+        frappe.throw("TelegramBot: {} do not exist".format(telegram_bot))
+
+    config = get_supervisor_config()
+
+    # Program
+    program_name, program = get_bot_program(
+        config=config, telegram_bot=telegram_bot, polling=polling, poll_interval=poll_interval,
+        webhook=webhook, webhook_port=webhook_port, webhook_url=webhook_url)
+
+    config[program_name] = program
+
+    # Bot Group
+    group_name = get_bot_group_name()
+    bot_programs = []
+    if group_name in config:
+        bot_programs = config[group_name]["programs"].split(",")
+
+    group_program_name = program_name.replace("program:", "")
+    if group_program_name not in bot_programs:
+        bot_programs.append(group_program_name)
+    config[group_name] = {"programs": ",".join(bot_programs)}
+
+    write_supervisor_config(config)
+
+
+def remove_supervisor_entry(telegram_bot):
+    config = get_supervisor_config()
+
+    # Remove Program Entry
+    program_name = get_bot_program_name(telegram_bot)
+    if program_name in config:
+        del config[program_name]
+
+    # Remove Group Entry
+    group_name = get_bot_group_name()
+    bot_programs = []
+    if group_name in config:
+        bot_programs = config[group_name]["programs"].split(",")
+
+    group_program_name = program_name.replace("program:", "")
+    if group_program_name in bot_programs:
+        bot_programs.remove(group_program_name)
+
+    if len(bot_programs):
+        config[group_name] = {"programs": ",".join(bot_programs)}
+    elif group_name in config:
+        del config[group_name]
+
+    write_supervisor_config(config)
+
+
+def get_bot_program(config, telegram_bot, **kwargs):
+    program_name = get_bot_program_name(telegram_bot)
+    logs = get_bot_log_paths(telegram_bot)
+
+    command = f"bench --site {frappe.local.site} telegram start-bot " + telegram_bot
+    for k, v in kwargs.items():
+        if not v:
+            continue
+        k = k.replace("_", "-")
+        if isinstance(v, bool):
+            command += f" --{k}"
+        else:
+            command += f" --{k} {v}"
+
+    program = {
+        "command": command,
+        "priority": 1,
+        "autostart": "true",
+        "autorestart": "true",
+        "stdout_logfile": logs[0],
+        "stderr_logfile": logs[1],
+        "user": guess_user_from_web_program(config=config),
+        "directory": os.path.abspath(get_site_path(".."))
+    }
+
+    return program_name, program
+
+
+def get_supervisor_config() -> configparser.ConfigParser():
+    supervisor_conf = os.path.join(get_bench_path(), "config", "supervisor.conf")
+    if not os.path.exists(supervisor_conf):
+        raise Exception(
+            "Please generate supervisor file using 'bench setup supervisor' before this action")
+
+    config = configparser.ConfigParser()
+    config.read(supervisor_conf)
+
+    return config
+
+
+def write_supervisor_config(config: configparser.ConfigParser):
+    supervisor_conf = os.path.join(get_bench_path(), "config", "supervisor.conf")
+    with open(supervisor_conf, 'w') as configfile:
+        config.write(configfile)
+
+    # TODO: Ask to restart supervisorctl or do automatically
+
+
+def guess_user_from_web_program(config: configparser.ConfigParser):
+    web_program_name = f"program:{get_bench_name()}-frappe-web"
+    if web_program_name not in config:
+        return
+
+    return config[web_program_name]["user"]
+
+
+def get_bot_log_paths(telegram_bot):
+    logs_path = os.path.abspath(os.path.join("..", "logs"))
+    stdout = os.path.join(logs_path, f"bot-{telegram_bot}.log")
+    stderr = os.path.join(logs_path, f"bot-{telegram_bot}.error.log")
+
+    return stdout, stderr
+
+
+def get_bot_program_name(telegram_bot):
+    return f"program:{get_bench_name()}-telegram-bot-{telegram_bot}"
+
+
+def get_bot_group_name():
+    return f"group:{get_bench_name()}-telegram-bots"
