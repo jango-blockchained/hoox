fafafce78467013d1feb990a5a3cfb6b480a97c6
jango_blockchained <jan.goischke@gmail.com>
2023-06-05 13:09:15 +0200

    Updated user.py

--- hoox/hoox/user.py
+++ hoox/hoox/user.py
@@ -1,13 +1,14 @@
 import frappe
 from frappe import _
-import telegram
+
 from frappe.utils.password import get_decrypted_password
 
 
 def get_exchange_credentials(secret_hash):
     exchange_creds = frappe.get_doc("Exchange Credentials", secret_hash)
     if not exchange_creds:
-        raise Exception(f"No exchange credentials found for secret hash {secret_hash}")
+        raise Exception(
+            f"No exchange credentials found for secret hash {secret_hash}")
     exchange_creds.api_key = get_decrypted_password(
         "Exchange Credentials", secret_hash, "api_key", False
     )
@@ -27,45 +28,86 @@ def get_haas_credentials(user):
     return haas_creds if haas_creds.enabled else None
 
 
-def send_to_telegram(user, message, toId=None):
-    settings = frappe.get_single("Hoox Settings")
-    bot_token = settings.telegram_bot_token if settings.telegram_enabled else None
+def send_to_telegram(user, message, settings, toId=None):
+
+    if not settings.telegram_enabled or not settings.telegram_bot_token:
+        return
+
     telegram_creds = frappe.get_doc("Telegram Credentials", {"user": user})
+    if not telegram_creds:
+        return
+
     chat_id = (
         toId or telegram_creds.user_id or telegram_creds.group_id
         if telegram_creds.enabled
         else None
     )
-    print(f"Telegram Bot Token: {bot_token}\nTelegram Chat ID: {chat_id}")
-    if bot_token and chat_id:
-        print(f"Sending to Telegram: {message}")
-        bot = telegram.Bot(token=bot_token)
-        bot.send_message(chat_id=chat_id, text=message)
-    else:
-        return None
+    bot = telegram.Bot(token=settings.telegram_bot_token)
+    response = bot.send_message(chat_id=chat_id, text=message)
+    print(f"Sending to Telegram: # {chat_id}\t{message}")
+
+    return response
 
 
-def send_to_haas(user, entity_id, service, data=None):
+def send_to_haas(user, entity_domain, service, data=None):
+    """
+    Sends a request to Home Assistant if there is a "haas" field in the request data.
+
+    Incoming << JSON format:
+    {
+        "secret_hash": "SECRET_HASH",
+        "haas": {
+            "entity_id": "light.living_room",
+            "service": "turn_on|turn_off|toggle|...",
+            * "data": {
+                * "rgb_color": [255, 255, 255],
+                * "brightness": 255,
+                * "color_temp": 255
+                * "effect": "colorloop|random|flash|...",
+            }
+        }
+    }
+    * Optional fields
+
+    Outgoing >> JSON format:
+    {
+        "entity_id": "light.living_room",
+        * "rgb_color": [255, 255, 255],
+        * "brightness": 255,
+        * "color_temp": 255
+        * "effect": "colorloop|random|flash|...",
+    }
+    """
+
+    if not user or not service or not entity_domain:
+        return
+
+    if not payload or not isinstance(payload, dict) or not payload.get("entity_id"):
+        return
+
+    import requests
+
     # Get Home Assistant credentials for the user from the DocType
     haas_creds = get_haas_credentials(user)
 
-    if haas_creds and haas_creds.enabled:
-        # Define the endpoint for the Home Assistant REST API
-        url = f"{haas_creds.ha_url}/api/services/{service}/{entity_id}"
+    if not haas_creds or not haas_creds.enabled:
+        return
 
-        # Define the headers for the API call
-        headers = {
-            "Authorization": f"Bearer {haas_creds.ha_user}:{haas_creds.ha_token}",
-            "Content-Type": "application/json",
-        }
+    # Define the endpoint for the Home Assistant REST API
+    url = f"{haas_creds.ha_url}/api/services/{entity_domain}/{service}"
 
-        # Make the API call
-        response = requests.post(url, headers=headers, data=data)
+    # Define the headers for the API call
+    headers = {
+        "Authorization": f"Bearer {haas_creds.ha_token}",
+        "Content-Type": "application/json",
+    }
 
-        # Check the response
-        if response.status_code != 200:
-            print(f"Failed to send to haas: {response.content}")
+    # Make the API call
+    response = requests.post(url, headers=headers, json=payload)
 
-        return response.content
-    else:
-        return None
+    # Check the response
+    if response.status_code != 200:
+        print(f"Failed to send to haas: {response.content}")
+        return
+
+    return response.content
