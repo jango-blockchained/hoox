03a6aa7aa16e49c2b91779a573d9c3adb98bb714
jango_blockchained <jan.goischke@gmail.com>
2023-06-23 02:12:58 +0000

    .

--- hoox/hoox/doctype/ccxt_exchanges/ccxt_exchanges.js
+++ hoox/hoox/doctype/ccxt_exchanges/ccxt_exchanges.js
@@ -1,31 +1,33 @@
 frappe.ui.form.on("CCXT Exchanges", {
   refresh: function (frm) {
-    frm.add_custom_button(__("Show Features"), function () {
-      let jsonContent = JSON.parse(frm.doc.has);
-      let Fields = [];
-      for (let key in jsonContent) {
-        if (jsonContent.hasOwnProperty(key)) {
-          let value = jsonContent[key];
-          Fields.push({
-            fieldtype: "Data",
-            label: key,
-            fieldname: key,
-            default: value,
-            read_only: true,
-          });
+    frm
+      .add_custom_button(__("Show Features"), function () {
+        let jsonContent = JSON.parse(frm.doc.has);
+        let Fields = [];
+        for (let key in jsonContent) {
+          if (jsonContent.hasOwnProperty(key)) {
+            let value = jsonContent[key];
+            Fields.push({
+              fieldtype: "Data",
+              label: key,
+              fieldname: key,
+              default: value,
+              read_only: true,
+            });
+          }
         }
-      }
-      let d = new frappe.ui.Dialog({
-        title: __("API Permissions"),
-        fields: Fields,
-        size: "small",
-        primary_action_label: __("Hide"),
-        primary_action(values) {
-          d.hide();
-        },
-      });
-      d.show();
-    });
+        let d = new frappe.ui.Dialog({
+          title: __("API Permissions"),
+          fields: Fields,
+          size: "small",
+          primary_action_label: __("Hide"),
+          primary_action(values) {
+            d.hide();
+          },
+        });
+        d.show();
+      })
+      .css({ "background-color": "#5525d8", color: "#fff" });
   },
 });
 
--- hoox/hoox/doctype/hoox_settings/hoox_settings.js
+++ hoox/hoox/doctype/hoox_settings/hoox_settings.js
@@ -1,14 +1,3 @@
 frappe.ui.form.on("Hoox Settings", {
-  refresh: function (frm) {
-    frm
-      .add_custom_button(__("Get Telegram Bot Token"), function () {
-        window.open("https://t.me/BotFather", "_blank");
-      })
-      .css({ "background-color": "#0088cc", color: "#fff" });
-    frm
-      .add_custom_button(__("Get Chat-ID"), function () {
-        window.open("https://t.me/myidbot", "_blank");
-      })
-      .css({ "background-color": "#0088cc", color: "#fff" });
-  },
+  refresh: function (frm) {},
 });
