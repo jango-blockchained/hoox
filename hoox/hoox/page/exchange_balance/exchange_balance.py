16a556a0d546c6ba67e9d834d104eb12e4443b00
jango-blockchained <info@inart-designs.decryptolinx.de>
2023-07-11 17:37:08 +0200

    Added file upload to decentralized IPFS Storage

--- hoox/hoox/page/exchange_balance/exchange_balance.py
+++ hoox/hoox/page/exchange_balance/exchange_balance.py
@@ -1,8 +1,29 @@
-import frappe
 from frappe import _
-import ccxt
+import frappe
+from hoox.hoox.doctype.
 
 
-def get_data():
+# Your controller or view function
+def balance_page():
+    # Fetch the data and create the `total` dictionary
+    # (Assuming you have already retrieved the data and stored it in the `data` variable)
 
-    return 
+    # # Extract the coin balances from the data
+    # coin_balances = data.get('total', {})
+
+    # Create the `total` dictionary with the required structure
+    # total = {}
+    # for coin, balance in coin_balances.items():
+    #     total[coin] = {
+    #         'balance': balance,
+    #         'free': data.get('free', {}).get(coin, 0),
+    #         'used': data.get('used', {}).get(coin, 0),
+    #         'debt': data.get('debt', {}).get(coin, 0)
+    #     }
+
+    # # Render the template with the context data
+    # context = {
+    #     'total': total
+    # }
+
+    return frappe.render_template('exchange_balance.html', context)
